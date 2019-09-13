d025e40bf529228ac3c196356a906025e0efa4a0
Luis M. García <luismago@gmail.com>
2019-09-13 05:00:16 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
-docker build -t luismago/multi-client -f ./client/Dockerfile ./client
-docker build -t luismago/multi-server -f ./server/Dockerfile ./server
-docker build -t luismago/multi-worker -f ./worker/Dockerfile ./worker
-docker push luismago/multi-client
-docker push luismago/multi-server
-docker push luismago/multi-worker
+docker build -t luismago/multi-client:latest -t luismago/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t luismago/multi-server:latest -t luismago/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t luismago/multi-worker:latest -t luismago/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push luismago/multi-client:latest
+docker push luismago/multi-server:latest
+docker push luismago/multi-worker:latest
+
+docker push luismago/multi-client:$GIT_SHA
+docker push luismago/multi-server:$GIT_SHA
+docker push luismago/multi-worker:$GIT_SHA
 kubectl apply -f k8s
-# -t luismago/multi-server:latest -t luismago/multi-server:$GIT_SHA
-#kubectl set image deployments/server-deployment server=luismago/multi-server:$GIT_SHA
-kubectl rollout restart deployment server-deployment
-kubectl rollout restart deployment client-deployment
-kubectl rollout restart deployment worker-deployment
+
+kubectl set image deployments/server-deployment server=luismago/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=luismago/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=luismago/multi-worker:$GIT_SHA
